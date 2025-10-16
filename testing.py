51139ff21a3f5ef05f1682a130874cbe984ee0bb
avan <100318118+avansear@users.noreply.github.com>
2025-10-15 23:54:09 -0400

    get median yield test cases all

--- testing.py
+++ testing.py
@@ -3,18 +3,44 @@ from main import get_median_yield, above_median, calc_avg_rain
 
 class TestCalc(unittest.TestCase):
     
-    #testing get_median_yield general case 1
+    #get_median_yield general case 1
     def test1(self):
         data = [
             {"Yield_tons_per_hectare": "5.0"},
             {"Yield_tons_per_hectare": "10.0"},
             {"Yield_tons_per_hectare": "7.0"}
         ]
+
         self.assertEqual(get_median_yield(data), 7.0)
 
-    #testing get_median_yield general case 2
+    #get_median_yield general case 2
     def test2(self):
-        
+        data = [
+            {"Yield_tons_per_hectare": "3.0"},
+            {"Yield_tons_per_hectare": "6.0"},
+            {"Yield_tons_per_hectare": "9.0"},
+            {"Yield_tons_per_hectare": "12.0"}
+        ]
+
+        self.assertEqual(get_median_yield(data), 7.5)
+
+    #get_median_yield edge case 1
+    def test3(self):
+        data = [
+            {"Yield_tons_per_hectare": "4.0"},
+            {"Yield_tons_per_hectare": "4.0"},
+            {"Yield_tons_per_hectare": "4.0"}
+        ]
+
+        self.assertEqual(get_median_yield(data), 4)
+    
+    #get_median_yield edge case 2
+    def test4(self):
+        data = [
+            {"Yield_tons_per_hectare": "15.0"}
+        ]
+
+        self.assertEqual(get_median_yield(data), 15)
 
 if __name__ == "__main__":
     unittest.main()
